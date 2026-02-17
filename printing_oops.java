bf438056c8ea34d127ad8205d55dd963d765f81e
Gurusai-kumar-Gajavalli <g.gskgajavalli@gmail.com>
2026-02-17 09:09:33 +0530

    printing oops

---
+++ printing_oops.java
@@ -0,0 +1,13 @@
+class  printing_oops {
+    public static void main(String[] args) {
+        System.out.println(" ********  " + " " + "  *******  " + " " + "  *******  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "*********  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("  *******   " + " " + " *******  " + " " + "**         " + " " + "*******");
+
+    }
+}
